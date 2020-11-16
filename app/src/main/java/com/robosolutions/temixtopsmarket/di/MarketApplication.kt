23518c0f45c0f67f676c8b8568b298dd5f8f859c
Sebastian <sebastianliando@gmail.com>
2020-11-16 17:48:38 +0800

    Add timber library

--- app/src/main/java/com/robosolutions/temixtopsmarket/di/MarketApplication.kt
+++ app/src/main/java/com/robosolutions/temixtopsmarket/di/MarketApplication.kt
@@ -1,7 +1,17 @@
 package com.robosolutions.temixtopsmarket.di
 
 import android.app.Application
+import com.robosolutions.temixtopsmarket.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class MarketApplication : Application()
+class MarketApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
