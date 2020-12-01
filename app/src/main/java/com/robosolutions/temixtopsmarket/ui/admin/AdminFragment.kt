f0047cf3e94c2913e59cf31933d317daa445fbd4
Sebastian <sebastianliando@gmail.com>
2020-12-01 22:24:46 +0800

    Fix admin panel transition

--- app/src/main/java/com/robosolutions/temixtopsmarket/ui/admin/AdminFragment.kt
+++ app/src/main/java/com/robosolutions/temixtopsmarket/ui/admin/AdminFragment.kt
@@ -1,7 +1,9 @@
 package com.robosolutions.temixtopsmarket.ui.admin
 
+import android.os.Bundle
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.transition.MaterialSharedAxis
 import com.robosolutions.temixtopsmarket.R
 import com.robosolutions.temixtopsmarket.databinding.FragmentAdminBinding
 import com.robosolutions.temixtopsmarket.extensions.completeHideTopBar
@@ -18,6 +20,13 @@ class AdminFragment : BindingViewModelFragment<FragmentAdminBinding, AdminFragme
 
     override val titleIdEn = R.string.title_admin_panel_en
     override val titleIdThai = R.string.title_admin_panel_th
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enterTransition = MaterialSharedAxis(MaterialSharedAxis.Z, true)
+        returnTransition = MaterialSharedAxis(MaterialSharedAxis.Z, false)
+    }
 
     fun showTemiTopBar() = lifecycleScope.launch {
         robot.showTopBar()
