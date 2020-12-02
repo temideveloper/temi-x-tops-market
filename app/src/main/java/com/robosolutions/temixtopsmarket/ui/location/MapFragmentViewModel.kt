4449f857db8505614e53c7119679ea08ee9c4649
Sebastian <sebastianliando@gmail.com>
2020-12-02 16:52:56 +0800

    Map displays locations that are not saved in temi, but is not selectable

--- app/src/main/java/com/robosolutions/temixtopsmarket/ui/location/MapFragmentViewModel.kt
+++ app/src/main/java/com/robosolutions/temixtopsmarket/ui/location/MapFragmentViewModel.kt
@@ -1,12 +1,12 @@
 package com.robosolutions.temixtopsmarket.ui.location
 
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.robosolutions.temixtopsmarket.preference.PreferenceRepository
+import com.robosolutions.temixtopsmarket.ui.base.LocationViewModel
 
 class MapFragmentViewModel @ViewModelInject constructor(
     repository: PreferenceRepository
-) : ViewModel() {
+) : LocationViewModel() {
     val zones = repository.locations.asLiveData()
 }
--- app/src/main/java/com/robosolutions/temixtopsmarket/ui/location/NavLocationFragmentViewModel.kt
+++ app/src/main/java/com/robosolutions/temixtopsmarket/ui/location/NavLocationFragmentViewModel.kt
@@ -4,11 +4,13 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.asLiveData
 import com.robosolutions.temixtopsmarket.preference.PreferenceRepository
 import com.robosolutions.temixtopsmarket.ui.base.LocationViewModel
+import kotlinx.coroutines.flow.take
 
 class NavLocationFragmentViewModel @ViewModelInject constructor(
     private val repository: PreferenceRepository
 ) : LocationViewModel() {
     val savedLocations = repository.locations.asLiveData()
+    val lastSavedLocations = repository.locations.take(1).asLiveData()
 
     fun saveZoneOne(location: String) = viewModelLaunch { repository.saveZoneOne(location) }
 
